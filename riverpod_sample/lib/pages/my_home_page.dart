cb644c30121d06abd6579048d2d39c56c4fec7e1
Takuya Sudo <sudo.takuya@kiroru-inc.jp>
2022-07-20 10:38:07 +0900

    ちょっと修正

--- riverpod_sample/lib/pages/my_home_page.dart
+++ riverpod_sample/lib/pages/my_home_page.dart
@@ -21,38 +21,51 @@ class MyHomePage extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text("title"),
       ),
-      body: Center(child:
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(profileNotifier.introduction),
-            ListCell(
-              title: myPosts.isEmpty ? '投稿なし' : myPosts.last,
-              buttonTitle: auth ? '投稿を増やす' : '会員だけが投稿できます',
-              handler: () {
-                if (!auth) return;
-                myPostsNotifier.add("投稿:No${myPosts.length + 1}");
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Text(profileNotifier.introduction),
+          ListCell(
+            title: myPosts.isEmpty ? '投稿なし' : myPosts.last,
+            buttonTitle: auth ? '投稿を増やす' : '会員だけが投稿できます',
+            handler: () {
+              if (!auth) return;
+              myPostsNotifier.add("投稿:No${myPosts.length + 1}");
+            }
+          ),
+          ListCell(
+            title: auth ? '認証済み' : '未認証',
+            buttonTitle: auth ? 'ログアウト' : 'ログイン',
+            handler: () async {
+              if (!auth) {
+                await authNotifier.login();
+                // ログイン後にプロフィールと投稿を取得（並列実行）
+                await Future.wait([
+                  profileNotifier.fetch(),
+                  myPostsNotifier.fetch(),
+                ]);
+              } else {
+                await authNotifier.logout();
               }
-            ),
-            ListCell(
-              title: auth ? '認証済み' : '未認証',
-              buttonTitle: auth ? 'ログアウト' : 'ログイン',
-              handler: () async {
-                if (!auth) {
-                  await authNotifier.login();
-                  // ログイン後にプロフィールと投稿を取得（並列実行）
-                  await Future.wait([
-                    profileNotifier.fetch(),
-                    myPostsNotifier.fetch(),
-                  ]);
-                } else {
-                  await authNotifier.logout();
-                }
-              },
-            ),
-          ],
-        ),
+            },
+          ),
+          ConstrainedBox(
+            constraints: BoxConstraints(maxHeight: 300),
+            child: ListView(
+              children: _getTile(myPosts),
+            )
+          ),
+        ],
       ),
     );
+  }
+
+  List<ListTile> _getTile(List<String> list) {
+    return list.map((e) => 
+      ListTile(
+        leading: Icon(Icons.favorite),
+        title: Text(e),
+      )
+    ).toList();
   }
 }
